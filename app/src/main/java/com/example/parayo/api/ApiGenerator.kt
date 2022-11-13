20644f78d90ea0a200a2a19af56c4026cddf94a5
PhilipY <philipy075@gmail.com>
2022-11-13 12:43:29 +0900

    서버 통신

---
+++ app/src/main/java/com/example/parayo/api/ApiGenerator.kt
@@ -0,0 +1,36 @@
+package com.example.parayo.api
+
+import android.os.Build.HOST
+import okhttp3.OkHttpClient
+import okhttp3.Protocol
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import java.util.*
+import java.util.concurrent.TimeUnit
+
+class ApiGenerator {
+    fun <T> generate(api: Class<T>): T = Retrofit.Builder()
+        .baseUrl(HOST)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(httpClient())
+        .build()
+        .create(api)
+
+    private fun httpClient() =
+        OkHttpClient.Builder().apply {
+            connectTimeout(100, TimeUnit.SECONDS)
+            addInterceptor(httpLoggingInterceptor())
+        }.build()
+
+    private fun httpLoggingInterceptor() =
+        HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    companion object {
+        //const val HOST = "http://10.0.2.2:8080"
+        const val HOST = "http://172.30.1.72:8080"
+    }
+}
--- app/src/main/java/com/example/parayo/intro/IntroActivity.kt
+++ app/src/main/java/com/example/parayo/intro/IntroActivity.kt
@@ -2,19 +2,30 @@ package com.example.parayo.intro
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import com.example.parayo.api.ParayoApi
+import kotlinx.coroutines.*
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.jetbrains.anko.setContentView
 import org.jetbrains.anko.startActivity
 
 class IntroActivity : Activity() {
 
+    val TAG = IntroActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        IntroActivityUI().setContentView(this)
+        val ui = IntroActivityUI()
+        ui.setContentView(this)
+
+        runBlocking {
+            try {
+                val response = ParayoApi.instance.hello()
+                Log.e(TAG, response.data)
+            } catch (e: java.lang.Exception){
+                Log.e(TAG, "Hello API 호출 오류", e)
+            }
+        }
     }
 }
 
