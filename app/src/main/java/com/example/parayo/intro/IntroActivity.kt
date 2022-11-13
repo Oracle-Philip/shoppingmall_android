7aca3bd983dd94b45800a5714edb366126784402
PhilipY <philipy075@gmail.com>
2022-11-13 11:48:00 +0900

    first commit

---
+++ app/src/main/java/com/example/parayo/intro/IntroActivity.kt
@@ -0,0 +1,25 @@
+package com.example.parayo.intro
+
+import android.app.Activity
+import android.os.Bundle
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.jetbrains.anko.setContentView
+import org.jetbrains.anko.startActivity
+
+class IntroActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
+
+
+
+
+
