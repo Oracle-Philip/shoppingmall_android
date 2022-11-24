922fd22efe540f687a84e2c96e1938f27bad327c
윤상필 <philipy075@btsoft.kr>
2022-11-24 20:45:43 +0900

    쇼핑몰앱 따라하기_ android token처리

--- app/src/main/java/com/example/parayo/signin/SigninActivity.kt
+++ app/src/main/java/com/example/parayo/signin/SigninActivity.kt
@@ -1,8 +1,17 @@
 package com.example.parayo.signin
 
+import android.os.Bundle
 import net.codephobia.ankomvvm.components.BaseActivity
+import org.jetbrains.anko.setContentView
 import kotlin.reflect.KClass
 
 class SigninActivity : BaseActivity<SigninViewModel>() {
     override val viewModelType = SigninViewModel::class
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        SigninActivityUI(getViewModel())
+            .setContentView(this)
+    }
 }
